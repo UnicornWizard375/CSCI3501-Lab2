118cebb1cb39f085be0a6c4e509e474136972079
unicornwizard375 <drumm040@morris.umn.edu>
2019-09-16 19:46:04 -0500

    finish insertquicksort, and update to have the program print out the number of comparisons for each type of sort. committed by Mitchell Drummer and Jonah Northwood.

--- src/InitializeArrays.java
+++ src/InitializeArrays.java
@@ -3,65 +3,157 @@ import java.util.Random;
 
 public class InitializeArrays {
         public static void main(String[] args) {
-        int length = 6;
-        TestInteger[] timSortArray = new TestInteger[length];
-        TestInteger[] quickSortArray = new TestInteger[length];
-        TestInteger[] randPArray = new TestInteger[length];
-        TestInteger[] medianArray = new TestInteger[length];
-        TestInteger[] insertArray = new TestInteger[length];
-        TestInteger counter = new TestInteger();
+            int length = 5000;
+            //use a length of multiples of 1000 or else the program won't work
+            //for me, length of 10000 gives me a stack overflow, so I changed it to 5000
 
-        for(int x = 0; x < length; x++){
-            TestInteger temp = GenerateTestIntegers();
-            timSortArray[x] = temp;
-            quickSortArray[x] = temp;
-            randPArray[x] = temp;
-            medianArray[x] = temp;
-            insertArray[x] = temp;
-        }
-
-
-        //sortedPresort(quickSortArray, timSortArray, length);
-        //presort10(quickSortArray, timSortArray);
-        //presort100(quickSortArray, timSortArray);
-
-
-        SortingAnalysis2ElectricBoogaloo.quickSort2ElectricBoogaloo(quickSortArray, 0, length-1);
-        System.out.println("Is quickSort sorted? " + isSorted(quickSortArray));
-        System.out.println("Number of comparisons for quick sort: " + counter.getCounter());
-
-
-        Arrays.sort(timSortArray);
-        System.out.println("Is timSort sorted? " + isSorted(timSortArray));
-        System.out.println("Number of comparisons for tim sort: " + counter.getCounter());
-
-        RandomizedQuicksort.randomQuicksortFunction(randPArray, 0, length-1);
-        System.out.println("Is random partition sort sorted? " + isSorted(randPArray));
-        System.out.println("Number of comparisons for random partition: " + counter.getCounter());
-
-        MedianQuicksort.medianQuicksortFunction(medianArray, 0, length-1);
-        System.out.println("Is median partition sort sorted? " + isSorted(medianArray));
-        System.out.println("Number of comparisons for median partition: " + counter.getCounter());
-
-        InsertQuicksort.insertQuicksortFunction(insertArray, 0, length-1);
-        System.out.println("Is insert partition sort sorted? " + isSorted(insertArray));
-        System.out.println("Number of comparisons for insert partition: " + counter.getCounter());
+            TestInteger[] timSortArray = new TestInteger[length];
+            TestInteger[] quickSortArray = new TestInteger[length];
+            TestInteger[] randPArray = new TestInteger[length];
+            TestInteger[] medianArray = new TestInteger[length];
+            TestInteger[] insertArray = new TestInteger[length];
+            TestInteger counter = new TestInteger();
 
 
 
-            //this clears the variable we use to count the comparisons
+            //the following loops print out the number of comparisons that were done for each form of sorting on each form of pre-sorted list
 
-            counter.getCounter();
+            System.out.println("\nStarting printing sorting results on random lists\n");
 
+//prints out the number of comparisons for each sorting type, 5 times, on a list that
+            for (int i = 1; i <= 5; i++) {
+                RandomList(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                SortingAnalysis2ElectricBoogaloo.quickSort2ElectricBoogaloo(quickSortArray, 0, length - 1);
+                System.out.println("Number of comparisons for quick sort on a random list, test number" + i + ": " + counter.getCounter());
+            }
 
+            for (int i = 1; i <= 5; i++) {
+                RandomList(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                Arrays.sort(timSortArray);
+                System.out.println("Number of comparisons for tim sort on a random list, test number" + i + ": " + counter.getCounter());
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                RandomList(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                RandomizedQuicksort.randomQuicksortFunction(randPArray, 0, length - 1);
+                System.out.println("Number of comparisons for random quicksort on a random list, test number" + i + ": " + counter.getCounter());
+            }
+            for (int i = 1; i <= 5; i++) {
+                RandomList(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                MedianQuicksort.medianQuicksortFunction(medianArray, 0, length - 1);
+                System.out.println("Number of comparisons for median quicksort on a random list, test number" + i + ": " + counter.getCounter());
+            }
+            for (int i = 1; i <= 5; i++) {
+                RandomList(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                InsertQuicksort.insertQuicksortFunction(insertArray, 0, length - 1);
+                System.out.println("Number of comparisons for insert quicksort on a random list, test number" + i + ": " + counter.getCounter());
+            }
+
+            System.out.println("\nStarting printing sorting results on list of increasing order\n");
+
+//tests for each form of sorting on a list of increasing order
+            for (int i = 1; i <= 5; i++) {
+                sortedPresort(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                SortingAnalysis2ElectricBoogaloo.quickSort2ElectricBoogaloo(quickSortArray, 0, length - 1);
+                System.out.println("Number of comparisons for quick sort on an increasing order list, test number" + i + ": " + counter.getCounter());
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                sortedPresort(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                Arrays.sort(timSortArray);
+                System.out.println("Number of comparisons for tim sort on an increasing order list, test number" + i + ": " + counter.getCounter());
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                sortedPresort(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                RandomizedQuicksort.randomQuicksortFunction(randPArray, 0, length - 1);
+                System.out.println("Number of comparisons for random quicksort on an increasing order list, test number" + i + ": " + counter.getCounter());
+            }
+            for (int i = 1; i <= 5; i++) {
+                sortedPresort(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                MedianQuicksort.medianQuicksortFunction(medianArray, 0, length - 1);
+                System.out.println("Number of comparisons for median quicksort on an increasing order list, test number" + i + ": " + counter.getCounter());
+            }
+            for (int i = 1; i <= 5; i++) {
+                sortedPresort(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                InsertQuicksort.insertQuicksortFunction(insertArray, 0, length - 1);
+                System.out.println("Number of comparisons for insert quicksort on an increasing order list, test number" + i + ": " + counter.getCounter());
+            }
+
+            System.out.println("\nStarting printing sorting results on 10 sequences of 1000 elements\n");
+
+            //sorting on a list composed of 10 sequences of 1000 element
+            for (int i = 1; i <= 5; i++) {
+                presort10(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                SortingAnalysis2ElectricBoogaloo.quickSort2ElectricBoogaloo(quickSortArray, 0, length - 1);
+                System.out.println("Number of comparisons for quick sort on the 10 sequences of 1000, test number" + i + ": " + counter.getCounter());
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                presort10(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                Arrays.sort(timSortArray);
+                System.out.println("Number of comparisons for tim sort on the 10 sequences of 1000, test number" + i + ": " + counter.getCounter());
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                presort10(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                RandomizedQuicksort.randomQuicksortFunction(randPArray, 0, length - 1);
+                System.out.println("Number of comparisons for random quicksort on the 10 sequences of 1000, test number" + i + ": " + counter.getCounter());
+            }
+            for (int i = 1; i <= 5; i++) {
+                presort10(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                MedianQuicksort.medianQuicksortFunction(medianArray, 0, length - 1);
+                System.out.println("Number of comparisons for median quicksort on the 10 sequences of 1000, test number" + i + ": " + counter.getCounter());
+            }
+            for (int i = 1; i <= 5; i++) {
+                presort10(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                InsertQuicksort.insertQuicksortFunction(insertArray, 0, length - 1);
+                System.out.println("Number of comparisons for insert quicksort on the 10 sequences of 1000, test number" + i + ": " + counter.getCounter());
+            }
+
+            System.out.println("\nStarting printing sorting results on 100 sequences of 100\n");
+            //100 sequences of 100 list
+            for (int i = 1; i <= 5; i++) {
+                presort100(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                SortingAnalysis2ElectricBoogaloo.quickSort2ElectricBoogaloo(quickSortArray, 0, length - 1);
+                System.out.println("Number of comparisons for quick sort on the 100 sequences of 100, test number" + i + ": " + counter.getCounter());
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                presort100(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                Arrays.sort(timSortArray);
+                System.out.println("Number of comparisons for tim sort on the 100 sequences of 100, test number" + i + ": " + counter.getCounter());
+            }
+
+            for (int i = 1; i <= 5; i++) {
+                presort100(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                RandomizedQuicksort.randomQuicksortFunction(randPArray, 0, length - 1);
+                System.out.println("Number of comparisons for random quicksort on the 100 sequences of 100, test number" + i + ": " + counter.getCounter());
+            }
+            for (int i = 1; i <= 5; i++) {
+                presort100(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                MedianQuicksort.medianQuicksortFunction(medianArray, 0, length - 1);
+                System.out.println("Number of comparisons for median quicksort on the 100 sequences of 100, test number" + i + ": " + counter.getCounter());
+            }
+            for (int i = 1; i <= 5; i++) {
+                presort100(quickSortArray, timSortArray, randPArray, medianArray, insertArray, length);
+                InsertQuicksort.insertQuicksortFunction(insertArray, 0, length - 1);
+                System.out.println("Number of comparisons for insert quicksort on the 100 sequences of 100, test number" + i + ": " + counter.getCounter());
+            }
     }
+
         //This is for pre-sorted list
-        public static void sortedPresort(TestInteger[] quickSortArray, TestInteger[] timSortArray, int length) {
+        public static void sortedPresort(TestInteger[] quickSortArray, TestInteger[] timSortArray,
+                                         TestInteger[] randPArray, TestInteger[] medianArray,
+                                         TestInteger[] insertArray,int length) {
         for (int x = 0; x < length; x++) {
             TestInteger temp = new TestInteger();
             temp.value = x;
             timSortArray[x] = temp;
             quickSortArray[x] = temp;
+            randPArray[x] = temp;
+            medianArray[x] = temp;
+            insertArray[x] = temp;
         }
         TestInteger temp2 = new TestInteger();
         temp2.getCounter();
@@ -69,10 +161,15 @@ public class InitializeArrays {
 
 
         //This was added for the 10 sequences of 1000 tests
-        public static void presort10(TestInteger[] quickSortArray, TestInteger[] timSortArray) {
-        for (int x = 0; x < 10; x++) {
-            Arrays.sort(quickSortArray, x * 1000, (x + 1) * 1000 - 1);
-            Arrays.sort(timSortArray, x * 1000, (x + 1) * 1000 - 1);
+        public static void presort10(TestInteger[] quickSortArray, TestInteger[] timSortArray,
+                                     TestInteger[] randPArray, TestInteger[] medianArray,
+                                     TestInteger[] insertArray,int length) {
+        for (int x = 0; x < (length / 1000); x++) {
+            Arrays.sort(quickSortArray, x * (length/10), (x + 1) * (length/10) - 1);
+            Arrays.sort(timSortArray, x * (length/10), (x + 1) * (length/10) - 1);
+            Arrays.sort(randPArray, x * (length/10), (x + 1) * (length/10) - 1);
+            Arrays.sort(medianArray, x * (length/10), (x + 1) * (length/10) - 1);
+            Arrays.sort(insertArray, x * (length/10), (x + 1) * (length/10) - 1);
         }
 
         TestInteger temp = new TestInteger();
@@ -83,18 +180,33 @@ public class InitializeArrays {
 
 
         //This was added for the 100 sequences of 100 tests
-        public static void presort100(TestInteger[] quickSortArray, TestInteger[] timSortArray) {
-        for (int x = 0; x < 100; x++) {
-            Arrays.sort(quickSortArray, x * 100, (x + 1) * 100 - 1);
-            Arrays.sort(timSortArray, x * 100, (x + 1) * 100 - 1);
+        public static void presort100(TestInteger[] quickSortArray, TestInteger[] timSortArray,
+                                      TestInteger[] randPArray, TestInteger[] medianArray,
+                                      TestInteger[] insertArray,int length) {
+        for (int x = 0; x < (length/100); x++) {
+            Arrays.sort(quickSortArray, x * (length/100), (x + 1) * (length/100) - 1);
+            Arrays.sort(timSortArray, x * (length/100), (x + 1) * (length/100) - 1);
+            Arrays.sort(randPArray, x * (length/100), (x + 1) * (length/100) - 1);
+            Arrays.sort(medianArray, x * (length/100), (x + 1) * (length/100) - 1);
+            Arrays.sort(insertArray, x * (length/100), (x + 1) * (length/100) - 1);
         }
 
         TestInteger temp = new TestInteger();
         temp.getCounter();
     }
 
-
-
+public static void RandomList (TestInteger[] quickSortArray, TestInteger[] timSortArray,
+                               TestInteger[] randPArray, TestInteger[] medianArray,
+                               TestInteger[] insertArray,int length) {
+    for (int x = 0; x < length; x++) {
+        TestInteger temp = GenerateTestIntegers();
+        quickSortArray[x] = temp;
+        timSortArray[x] = temp;
+        randPArray[x] = temp;
+        medianArray[x] = temp;
+        insertArray[x] = temp;
+    }
+}
 
 
 
--- src/InsertQuicksort.java
+++ src/InsertQuicksort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 
 public class InsertQuicksort {
-public static int k = 10;
+public static int k = 30;
 
     public static void insertQuicksortFunction(TestInteger[] A, int p, int r){
             if (p < r && ((r - p) > k)) {
@@ -22,8 +22,6 @@ public static int k = 10;
         int i = p - 1;
         TestInteger temp;
 
-//        if (r - p > k) {
-
             for (int j = p; j <= r - 1; j++) {
                 if (A[j].compareTo(x) <= 0) {
                     i = i + 1;
@@ -37,18 +35,13 @@ public static int k = 10;
             A[i + 1] = A[r];
             A[r] = temp;
             return i + 1;
-//        }
-//        else {
-//        insertFunction(A, p, r);
-//        return i + 1;
-//        }
     }
 
     private static void insertFunction(TestInteger[] A, int p, int r){
-        for(int j= p+1; j < r - p - 1; j++) {
+        for(int j= p+1; j <= r; j++) {
             TestInteger key = A[j];
             int i = j-1;
-            while (i >= 0 && (A[i].compareTo(key) > 0)){
+            while (i >= p && (A[i].compareTo(key) > 0)){
                 A[i+1] = A[i];
                 i = i-1;
             }
